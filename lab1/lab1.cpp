006a8ffc898fa1f2c06b07f5e16e2d54d93a435c
DLaaren <100743455+DLaaren@users.noreply.github.com>
2023-04-04 13:08:06 +0700

    зарядил тесты
    Старый дедосвкий способ решить уравнение итерационным методом! Нужно всего лишь...

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -2,28 +2,30 @@
 #include <cmath>
 #include <climits>
 #include <stdlib.h>
+#include <stdio.h>
 #include <mpi.h>
 
-#define N 50
-#define EPSILON 0.00001 //10^(-5)
-#define PARAMETER 0.01
+#define N 2500
+#define EPSILON 0.01 //10^(-5)
+#define PARAMETER -0.1
 
-double randDouble() {
-    return ( (double)rand() / RAND_MAX ) * 4.0 - 2.0;
+float randDouble() {
+    return ( (float)rand() / RAND_MAX ) * 3.0 - 2.0;
 }
 
-void generateMatrix(double *M, int columns, int rows) {
+void generateMatrix(float *M, int columns, int rows) {
     for (int i = 0; i < columns; i++) {
         for (int j = 0; j < rows; j++) {
             M[i*rows + j] = randDouble();
-            if (i == j) {
+            //if you do not use tests
+            /*if (i == j) {
                 M[i*rows + j] = fabs(M[i*rows + j]) + 124.0; //выполнение условия диагонального преобладания
-            }
+            }*/
         }
     }
 }
 
-void mul(double *M1, double *M2, double *resM, int size) {
+void mul(float *M1, float *M2, float *resM, int size) {
     for (int i = 0; i < size; i++) {
         resM[i] = 0;
         for (unsigned int j = 0; j < N; j++) {
@@ -32,20 +34,20 @@ void mul(double *M1, double *M2, double *resM, int size) {
     }
 }
 
-void scalMult(double *M, double scalar, int size) {
+void scalMult(float *M, float scalar, int size) {
     for (int i = 0; i < size; i++) {
         M[i] *= scalar;
     }
 }
 
-void sub(const double *M1, const double *M2, double *resM, int size) {
+void sub(const float *M1, const float *M2, float *resM, int size) {
     for (int i = 0; i < size; i++) {
         resM[i] = M1[i] - M2[i];
     }
 }
 
-double preEuclideanNorm(const double* M, int size) {
-    double norm = 0;
+float preEuclideanNorm(const float* M, int size) {
+    float norm = 0;
     for (int i = 0; i < size; i++) {
         norm += M[i]*M[i];
     }
@@ -59,7 +61,7 @@ void copyVector(const T* M1, T* M2, int size) {
     }
 }
 
-void printVector(double* vector, int size) {
+void printVector(float* vector, int size) {
     printf("\n");
     for (int i = 0; i < size; i++) {
         printf("%lf ", vector[i]);
@@ -67,7 +69,7 @@ void printVector(double* vector, int size) {
     printf("\n");
 }
 
-void printMatrix(double* M, int columns, int rows) {
+void printMatrix(float* M, int columns, int rows) {
     printf("\n");
     for (int i = 0; i < columns; i++) {
         for (int j = 0; j < rows; j++) {
@@ -122,63 +124,95 @@ int main() {
 
     int sizeOfBuffers = numberOfElementsVector[processRank];
 
-    double* A_buffer = (double*)malloc(sizeof(double) * N * sizeOfBuffers);
-    double* Ax_buffer = (double*)malloc(sizeof(double) * sizeOfBuffers);
-    double* b_buffer = (double*)malloc(sizeof(double) * sizeOfBuffers);
+    float* A_buffer = (float*)malloc(sizeof(float) * N * sizeOfBuffers);
+    float* Ax_buffer = (float*)malloc(sizeof(float) * sizeOfBuffers);
+    float* b_buffer = (float*)malloc(sizeof(float) * sizeOfBuffers);
 
     //all threads:
-    double* A;
-    double* b;
-    double* prevX = (double*)malloc(sizeof(double) * N);
-    double* nextX;
-    double* Ax;
-    double norm_b;
-    double norm_Ax;
-    double tau = PARAMETER;
-    double result = INT_MAX;
-    double lastResult = INT_MAX;
+    float* A;
+    float* b;
+    float* prevX = (float*)malloc(sizeof(float) * N);
+    float* nextX;
+    float* Ax;
+    float norm_b;
+    float norm_Ax;
+    float tau = PARAMETER;
+    float result = INT_MAX;
+    float lastResult = 0;
     unsigned int itCount = 0;
 
     //main thread:
     if (processRank == 0) {
-        A = (double*)malloc(sizeof(double) * N * N);
-        generateMatrix(A, N, N);
+        A = (float*)malloc(sizeof(float) * N * N);
+        //if you do nit use tests
+        //generateMatrix(A, N, N);
+        FILE* Abin = fopen("matA.bin", "r");
+        if (Abin == NULL) {
+            perror("fopen()");
+            free(numberOfElementsVector);
+            free(arrayOffsetsVector);
+            free(numberOfElementsMatrix);
+            free(arrayOffsetsMatrix);
+            free(A_buffer);
+            free(Ax_buffer);
+            free(b_buffer);
+            free(prevX);
+            return -1;
+        }
+        fread(A, 4, N * N, Abin);
+        fclose(Abin);
 
-        Ax = (double*)malloc(sizeof(double) * N);
+        Ax = (float*)malloc(sizeof(float) * N);
 
-        b = (double*)malloc(sizeof(double) * N);
-        generateMatrix(b, N, 1);
+        b = (float*)malloc(sizeof(float) * N);
+        //generateMatrix(b, N, 1);
+        FILE* Bbin = fopen("vecB.bin", "r");
+        if (Bbin == NULL) {
+            perror("fopen()");
+            free(numberOfElementsVector);
+            free(arrayOffsetsVector);
+            free(numberOfElementsMatrix);
+            free(arrayOffsetsMatrix);
+            free(A_buffer);
+            free(Ax_buffer);
+            free(b_buffer);
+            free(prevX);
+            return -1;
+        }
+        fread(b, 4, N, Bbin);
+        fclose(Bbin);
 
-        nextX = (double*)malloc(sizeof(double) * N);
+        nextX = (float*)malloc(sizeof(float) * N);
         generateMatrix(nextX, N, 1);
 
         norm_b = preEuclideanNorm(b, N);
         norm_b = sqrt(norm_b); // ||b||
     }
-    MPI_Bcast(prevX, N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    MPI_Scatterv(b, numberOfElementsVector, arrayOffsetsVector, MPI_DOUBLE, b_buffer, sizeOfBuffers, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    MPI_Scatterv(A, numberOfElementsMatrix, arrayOffsetsMatrix, MPI_DOUBLE, A_buffer, N * sizeOfBuffers, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    double start = MPI_Wtime();
+    MPI_Bcast(prevX, N, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    MPI_Scatterv(b, numberOfElementsVector, arrayOffsetsVector, MPI_FLOAT, b_buffer, sizeOfBuffers, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    MPI_Scatterv(A, numberOfElementsMatrix, arrayOffsetsMatrix, MPI_FLOAT, A_buffer, N * sizeOfBuffers, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+    float start = MPI_Wtime();
     while (result > EPSILON) {
 
         if (processRank == 0) {
-            copyVector<double>(nextX, prevX, N);
+            copyVector<float>(nextX, prevX, N);
         }
         
-        MPI_Bcast(prevX, N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Bcast(prevX, N, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
         mul(A_buffer, prevX, Ax_buffer, sizeOfBuffers); // Ax_buf = A_buf * prevX; 
 
         sub(Ax_buffer, b_buffer, Ax_buffer, sizeOfBuffers); // Ax_buf - b_buf
 
-        double preNorm_Ax = preEuclideanNorm(Ax_buffer, sizeOfBuffers); // (Ax_buf - b_buf)^2
+        float preNorm_Ax = preEuclideanNorm(Ax_buffer, sizeOfBuffers); // (Ax_buf - b_buf)^2
 
-        MPI_Reduce(&preNorm_Ax, &norm_Ax, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD); //adding all prenorms in the root
+        MPI_Reduce(&preNorm_Ax, &norm_Ax, 1, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD); //adding all prenorms in the root
 
         scalMult(Ax_buffer, tau, sizeOfBuffers); // tau * (Ax_buf - b_buf)
         
-        MPI_Gatherv(Ax_buffer, sizeOfBuffers, MPI_DOUBLE, Ax, numberOfElementsVector, arrayOffsetsVector, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Gatherv(Ax_buffer, sizeOfBuffers, MPI_FLOAT, Ax, numberOfElementsVector, arrayOffsetsVector, MPI_FLOAT, 0, MPI_COMM_WORLD);
         
         if (processRank == 0) {
             sub(prevX, Ax, nextX, N); // prevX - tau * (Ax - b) = nextX
@@ -187,11 +221,12 @@ int main() {
             result = norm_Ax / norm_b; //  ||Ax - b|| / ||b||
             itCount++;
 
-            if ((itCount >= 1000 && lastResult > result) || result == INFINITY) {
-                if (tau < 0) {
+            if ((itCount >= 1000 && lastResult < result) || result == INFINITY) {
+                if (tau > 0) {
                     printf("Doesn't have solution\n");
                     result = 0;
                 } else {
+                    printf("\nchanged tau\n");
                     tau = -PARAMETER;
                     result = INT_MAX;
                     itCount = 0;
@@ -199,18 +234,46 @@ int main() {
             }
             lastResult = result;
         }
-        MPI_Bcast(&result, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD); //for checking in the while
-        MPI_Barrier(MPI_COMM_WORLD); //synchronization for all threads
+        MPI_Bcast(&result, 1, MPI_FLOAT, 0, MPI_COMM_WORLD); //for checking in the while
+        //we don't need MPI_Barrier, because MPI_Bcast synchronize all processes
+        //MPI_Barrier(MPI_COMM_WORLD); //synchronization for all threads
     }
-    double end = MPI_Wtime();
+    float end = MPI_Wtime();
 
     printf("time: %lf\n", end - start);
+
+    float* answerX;
+    if (processRank == 0) {
+        answerX = (float*)malloc(sizeof(float) * N);
+        FILE* Xbin = fopen("vecX.bin", "r");
+        if (Xbin == NULL) {
+            perror("fopen()");
+            free(numberOfElementsVector);
+            free(arrayOffsetsVector);
+            free(numberOfElementsMatrix);
+            free(arrayOffsetsMatrix);
+            free(A_buffer);
+            free(Ax_buffer);
+            free(b_buffer);
+            free(prevX);
+            free(answerX);
+            return -1;
+        }
+        fread(answerX, 4, N, Xbin);
+        fclose(Xbin);
+
+        FILE* resultX = fopen("result.bin", "w");
+        fwrite(nextX, 4, N, resultX);
+        fclose(resultX);
+    }
+
 
     if (processRank == 0) {
         free(A);
         free(b);
         free(Ax);
         free(nextX);
+        free(answerX);
     }
     free(numberOfElementsVector);
     free(numberOfElementsMatrix);
