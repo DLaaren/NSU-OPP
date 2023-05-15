001ffa808ceb6fe6aeb289fe72f70029d9bbdf42
DLaaren <100743455+DLaaren@users.noreply.github.com>
2023-05-15 22:52:39 +0700

    Add files via upload

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -4,7 +4,9 @@
 #include <cfloat>
 #include <cstring>
 
-#define N 100
+using namespace std;
+
+#define N 320
 #define a 10e5
 #define epsilon 10e-8
 
@@ -25,32 +27,36 @@ static double Hz = Dz / (double)(N-1);
 static int commSize;
 static int processRank;
 
-void printCube(double* M) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            for (int k = 0; k < N; k++) {
-                    printf(" %7.4lf", M[point(i,j,k)]);
-            }
-            printf(";");
-        }
-        printf("\n");
-    }
-}
-
-double get_Xi(int i) {
+double get_X(int i) {
     return Xo + i * Hx;
 }
 
-double get_Yj(int j) {
+double get_Y(int j) {
     return Yo + j * Hy;
 }
 
-double get_Zk(int k) {
+double get_Z(int k) {
     return Zo + k * Hz;
 }
 
 double fi_function(double x, double y, double z) {
     return x*x + y*y + z*z;
+}
+
+double delta(double* omega){
+    auto delta = DBL_MIN;
+    double x, y, z;
+    for(int i = 0; i < N; i++){
+        z = get_Z(i);
+        for (int j = 0; j < N; j++){
+            x = get_X(j);
+            for(int k = 0; k < N; k++){
+                y = get_Y(k);
+                delta = max(delta, abs(omega[point(i,j,k)] - fi_function(x, y, z)));
+            }
+        }
+    }
+    return delta;
 }
 
 double ro_fucntion(double x, double y, double z) {
@@ -59,19 +65,19 @@ double ro_fucntion(double x, double y, double z) {
 
 //шаг 1-2 :: задать значения искомой функции на границе и внутри области омега
 void init_fi_function(int layerSize, double* currLayer) {
-    int start_Z_for_each_process = processRank * layerSize - 1;
-    for (int k = 0; k < layerSize + 2; k++) {
-        double z = get_Zk(k + start_Z_for_each_process);     
+    int relative_Z_coord = processRank * layerSize - 1;
+    for (int i = 0; i < layerSize + 2; i++) {
+        double z = get_Z(i + relative_Z_coord);     
 
-        for (int i = 0; i < N; i++) {
-            double x = get_Xi(i);
+        for (int j = 0; j < N; j++) {
+            double x = get_X(j);
 
-            for (int j = 0; j < N; j++) {
-                double y = get_Yj(j);
+            for (int k = 0; k < N; k++) {
+                double y = get_Y(k);
 
                 if (k != 0 && k != N-1 &&
                     j != 0 && j != N-1 &&
-                    i != 0 && i != N-1) {
+                    z != Zo && z != Zo + Dz) {
                         //inside
                         currLayer[point(i,j,k)] = 0;
                 } else {
@@ -84,8 +90,42 @@ void init_fi_function(int layerSize, double* currLayer) {
     }
 }
 
-double updateLayer() {
+double updateLayer(int relative_Z_coord, int layerIndex, double* currLayer, double* currLayerBuf) {
+    double x, y, z;
+    int absolute_Z_coord = relative_Z_coord + layerIndex;
+    auto delta = DBL_MIN;
 
+    if (absolute_Z_coord == 0 || absolute_Z_coord == N-1) {
+        //border parts -> do not update
+        std::memcpy(currLayerBuf + layerIndex * N * N, currLayer + layerIndex * N * N, N * N * sizeof(double));
+        delta = 0;
+    } else {
+        z = get_Z(absolute_Z_coord);
+        
+        for (int i = 0; i < N; i++) {
+            x = get_X(i);
+
+            for (int j = 0; j < N; j++) {
+                y = get_Y(j);
+
+                if (i == 0 || i == N-1 || j == 0 || j == N-1) {
+                    //border points --> just copy
+                    currLayerBuf[point(layerIndex, i, j)] = currLayer[point(layerIndex, i, j)];
+                } else {
+                    currLayerBuf[point(layerIndex, i, j)] =
+                            ((currLayer[point(layerIndex+1, i, j)] + currLayer[point(layerIndex-1, i, j)]) / (Hz*Hz) +
+                             (currLayer[point(layerIndex, i+1, j)] + currLayer[point(layerIndex, i-1, j)]) / (Hx*Hx) +
+                             (currLayer[point(layerIndex, i, j+1)] + currLayer[point(layerIndex, i, j-1)]) / (Hy*Hy) -
+                             ro_fucntion(x, y, z)) / (2/(Hx*Hx) + 2/(Hy*Hy) + 2/(Hz*Hz) + a);
+                
+                     if (abs(currLayerBuf[point(layerIndex, i, j)] - currLayer[point(layerIndex, i, j)]) > delta){
+                        delta = currLayerBuf[point(layerIndex, i, j)] - currLayer[point(layerIndex, i, j)];
+                    }
+                }
+            }
+        }
+    }
+    return delta;
 }
 
 int main() {
@@ -93,13 +133,15 @@ int main() {
     MPI_Comm_size(MPI_COMM_WORLD, &commSize);
     MPI_Comm_rank(MPI_COMM_WORLD, &processRank);
 
-    if (N % commSize && processRank == 0) {
+    if (N % commSize != 0) {
         std::cout << "Invalid grid size\n";
         return 0;
     }
 
+    auto globalDelta = DBL_MAX;
+
     int layerSize = N / commSize;
-    int layerCoord_indexZ = processRank * layerSize + Zo;
+    int relative_Z_coord_forLayer = processRank * layerSize - 1;
     //объем больше одного блока, т к будем брать краи соседей --> +2 (сверху и снизку по +1)
     int layerVolume = (layerSize + 2) * N * N;
     double* currLayer = new double[layerVolume];
@@ -110,34 +152,52 @@ int main() {
     double start = MPI_Wtime();
 
     init_fi_function(layerSize, currLayer);
-
-    double globalDelta = DBL_MAX;
     do {
         //send and receive in background
         if (processRank != 0) {
-            //lower part of layer
+            //lower border parts
             MPI_Isend(currLayerBuf + N*N, N*N, MPI_DOUBLE, processRank - 1, 123, MPI_COMM_WORLD, &req[0]);
             MPI_Irecv(currLayerBuf, N*N, MPI_DOUBLE, processRank - 1, 123, MPI_COMM_WORLD, &req[1]);
         }
 
         if (processRank != commSize - 1) {
-            //upper part of layer
-            MPI_Isend(currLayerBuf + layerSize * N*N, N*N, MPI_DOUBLE, processRank + 1, 123, MPI_COMM_WORLD, &req[2]);
-            MPI_Irecv(currLayerBuf + (layerSize + 1) * N*N, N*N, MPI_DOUBLE, processRank + 1, 123, MPI_COMM_WORLD, &req[3]);
+            //upper border parts
+            MPI_Isend(currLayerBuf + N*N*layerSize, N*N, MPI_DOUBLE, processRank + 1, 123, MPI_COMM_WORLD, &req[2]);
+            MPI_Irecv(currLayerBuf + N*N*(layerSize+1), N*N, MPI_DOUBLE, processRank + 1, 123, MPI_COMM_WORLD, &req[3]);
         }
 
-        double localDelta = DBL_MIN;
+        auto localDelta = DBL_MIN;
         double tmpDelta;
 
-        //update layers
-        for () {
-            updateLayer();
+        //update layers for each Z_k; k = 1 ... layerSize - 1;
+        for (int indexLayer = 2; indexLayer < layerSize; indexLayer++) {
+            tmpDelta = updateLayer(relative_Z_coord_forLayer, indexLayer, currLayer, currLayerBuf);
+            localDelta = max(tmpDelta, localDelta);
         }
 
         //wait for upper and lower parts
         //MPI_Wait()
+        if (processRank != 0) {
+            MPI_Wait(&req[0], MPI_STATUS_IGNORE);
+            MPI_Wait(&req[1], MPI_STATUS_IGNORE);
+        }
 
-        //update these parts
+        if (processRank != commSize - 1) {
+            MPI_Wait(&req[2], MPI_STATUS_IGNORE);
+            MPI_Wait(&req[3], MPI_STATUS_IGNORE);
+        }
+
+        //update received border parts
+
+        //for Z_k = 1
+        tmpDelta = updateLayer(relative_Z_coord_forLayer, 1, currLayer, currLayerBuf);
+        localDelta = max(tmpDelta, localDelta);
+
+        //for Z_k = layerSize;
+        tmpDelta = updateLayer(relative_Z_coord_forLayer, layerSize, currLayer, currLayerBuf);
+        localDelta = max(tmpDelta, localDelta);
+
+        memcpy(currLayer, currLayerBuf, layerVolume * sizeof(double));
 
         //gatherAll
         MPI_Allreduce(&localDelta, &globalDelta, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
@@ -150,16 +210,15 @@ int main() {
         omega = new double[N * N * N];
     }
 
-    MPI_Gather();
+    // + N*N <--- do not forget about empty border parts
+    MPI_Gather(currLayer + N*N, layerSize * N*N, MPI_DOUBLE, omega,
+               layerSize * N*N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
     double end = MPI_Wtime();
 
     if (processRank == 0) {
-        std::cout << "Time: " << end - start << "\n";
-    }
-
-    if (processRank == 0) {
-        printCube(omega);
+        cout << "Time: " << end - start << "\n";
+        cout << "Delta: " << delta(omega) << "\n";
         delete [] omega;
     }
 
